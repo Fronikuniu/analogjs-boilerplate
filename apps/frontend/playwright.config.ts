5f890c636df729da72d0ea4fbc578505973eaa57
Fronikuniu <Fronikuniu@gmail.com>
2024-03-12 21:28:03 +0100

    Change database from sqlite to postgres

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -23,12 +23,13 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
-  /* Run your local dev server before starting the tests */ // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  //   cwd: workspaceRoot,
-  // },
+  /* Run your local dev server before starting the tests */
+  webServer: {
+    command: "npx nx run frontend:serve",
+    url: baseURL,
+    reuseExistingServer: !process.env["CI"],
+    cwd: workspaceRoot,
+  },
   projects: [
     {
       name: "chromium",
