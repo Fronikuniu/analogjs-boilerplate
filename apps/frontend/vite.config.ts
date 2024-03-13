ab6c4caf95e4cdc357c89b0889ecb6e624a2f3d5
Fronikuniu <Fronikuniu@gmail.com>
2024-03-13 19:39:03 +0100

    Add type imports rule

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 
 import analog from "@analogjs/platform";
-import { defineConfig, Plugin, splitVendorChunkPlugin } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import nitro from "@analogjs/vite-plugin-nitro";
 
